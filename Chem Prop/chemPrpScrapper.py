5b793f8dde3e573e1e71e7d7f6ea1a8799793197
Gawquon <michael.l.whitehurst@vanderbilt.edu>
2018-06-12 16:17:31 -0500

    Fixed problem with float inputs

--- Chem Prop/chemPrpScrapper.py
+++ Chem Prop/chemPrpScrapper.py
@@ -13,20 +13,22 @@ from selenium.webdriver.support.ui import Select, WebDriverWait
 # This program requires that a chromedriver is installed and that the driver is in the PATH
 compound = 'butane' #compound of intrest as it appears in the NIST menu
 temperature = 375 #T in Kelvin you want to look at
-pressure_min = 0 #MPa minimum pressure to look at
-pressure_max = 100 #MPa maximum pressure to look at
-x_property = 'Pressure' # property wanted on the x-axis as listed in NIST
-y_property = 'Volume' # property wanted on the y-axis as listed in NIST
+pressure_min = 0.08 #MPa minimum pressure to look at
+pressure_max = 0.12 #MPa maximum pressure to look at
+x_property = 'Density' # property wanted on the x-axis as listed in NIST
+y_property = 'Pressure' # property wanted on the y-axis as listed in NIST
 ###
 
 ### Function declarations
 def clean_str_HTML(html_string):
 	"""This takes a string and returns it with all HTML tags (i.e </sub>) removed."""
 	h = html_string
-	h=h.replace('/','')
 	h=h.replace("<br>",'')
 	h=h.replace("<sub>",'')
 	h=h.replace("<sup>",'')
+	h=h.replace("</br>",'')
+	h=h.replace("</sub>",'')
+	h=h.replace("</sup>",'')
 	return h
 
 def substr_in_list(sub,list,fetch=False):
@@ -47,19 +49,20 @@ try:
 	options.add_argument('--disable-gpu') #windows only -- can delete otherwise
 	#This program requires that a chromedriver is installed and that the driver is in the PATH
 
-	driver = webdriver.Chrome('/mnt/d/TT-RPG/Code/PythonScripts/NPCgen/chromedriver_win32/chromedriver.exe',chrome_options=options) #linux
+	driver = webdriver.Chrome('.../chromedriver.exe',chrome_options=options) #linux
 	driver.get('https://webbook.nist.gov/chemistry/fluid/')
 	###
 
 	### First Page
 	driver.find_element_by_xpath('//*[@id="ID"]').send_keys(compound) #Molecule selector ID
 	driver.find_element_by_xpath('//*[@id="main"]/form/ol/li[5]/input[1]').click()
+	#driver.find_element_by_xpath('//*[@id="main"]/form/ol/li[2]/div/table/tbody/tr[6]/td/fieldset/label[3]/input').click()
 	###
 	time.sleep(3) # Should eventualy be replaced with a proper wait method
 	### Second Page
-	driver.find_element_by_xpath('//*[@id="main"]/form/ol/li[1]/input').send_keys(temperature) # T input
-	driver.find_element_by_xpath('//*[@id="main"]/form/ol/li[2]/table/tbody/tr[1]/td[1]/input').send_keys(pressure_min) # PLow input
-	driver.find_element_by_xpath('//*[@id="main"]/form/ol/li[2]/table/tbody/tr[2]/td[1]/input').send_keys(pressure_max) # PHigh input
+	driver.find_element_by_xpath('//*[@id="main"]/form/ol/li[1]/input').send_keys(str(temperature)) # T input
+	driver.find_element_by_xpath('//*[@id="main"]/form/ol/li[2]/table/tbody/tr[1]/td[1]/input').send_keys(str(pressure_min)) # PLow input
+	driver.find_element_by_xpath('//*[@id="main"]/form/ol/li[2]/table/tbody/tr[2]/td[1]/input').send_keys(str(pressure_max)) # PHigh input
 	driver.find_element_by_xpath('//*[@id="main"]/form/ol/li[3]/input').click() # checkbox
 	driver.find_element_by_xpath('//*[@id="main"]/form/ol/li[5]/input[1]').click()
 	###
@@ -78,6 +81,7 @@ try:
 	df_scaffold = []
 	df_scaffold.append([])
 	df_scaffold.append([])
+
 	for row in table.find_elements_by_css_selector('tr'):
 		d = row.find_elements_by_css_selector('td')
 		if(d != []):
@@ -92,8 +96,12 @@ except Exception as e:
 
 df = pd.DataFrame(df_scaffold).T
 df.columns = [headers[x_index], headers[y_index]]
+df = df.convert_objects(convert_numeric=True,)
+df = df.dropna(axis=0)
 
-df.plot.scatter(x=headers[x_index],y=headers[y_index],title=compound + " at " + temperature + "K",s=50)
+ml.use("Agg")
+
+df.plot.scatter(x=headers[x_index],y=headers[y_index],title=compound + " at " + str(temperature) + "K",s=15)
 
 ml.pyplot.savefig(compound)
 ###
